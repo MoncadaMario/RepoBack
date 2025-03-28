7fa16e38a6a3293a767be007be1634d38b857d34
DESKTOP-BHK1VBM\memon <memoncada05@gmail.com>
2025-03-27 18:29:33 -0600

    creacion de proyecto

---
+++ RepoBackEnd/RepoBackEnd/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RepoBackEnd.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
